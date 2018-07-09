f6f4fc8f6b12d86628e5047a8c33efc8eda4b375
inwolf <brianchen501@gmail.com>
2018-07-09 11:45:15 +0800

    page: partially add mock-data for pagination display

--- src/views/ComicPage/index.vue
+++ src/views/ComicPage/index.vue
@@ -1,7 +1,30 @@
 <script>
+import { chapters } from "@/mock-data";
+
 export default {
-  name: 'page'
-}
+  name: "page",
+  data() {
+    return {
+      cid: this.$route.params.cid,
+      chapters: chapters
+    };
+  },
+  created() {
+    console.log(this.cid);
+    console.log(this.chapters);
+    
+  },
+  computed: {
+    chapter() {
+      return this.chapters.filter(obj => obj.no === this.cid)[0]
+    }
+  },
+  methods: {
+    alertNotImplemented() {
+      alert('not implemented yet')
+    }
+  }
+};
 </script>
 
 <template lang="pug" src="./template.pug"/>
